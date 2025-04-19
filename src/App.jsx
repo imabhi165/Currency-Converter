818e6c5403483b929fa2e9624826c9c58dcfb4a3
Abhishek Kumar <144894858+imabhi165@users.noreply.github.com>
2025-04-19 10:21:47 +0530

    Update App.jsx
    Bcg changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import CurrencyConvertor from "./components/currency-convertor";
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-900 flex flex-col items-center justify-center">
+    <div className="min-h-screen bg-slate-950 flex flex-col items-center justify-center">
       <div className="container">
         <CurrencyConvertor />
       </div>
