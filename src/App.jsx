765778b33bfcdbb75861dd5f8bd39cab01fa5e4b
Abhishek <abhishek.k@avench.com>
2025-08-26 10:13:41 +0530

    trying to add shadcn

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ function App() {
   return (
     <div className="h-screen bg-gradient-to-r from-blue-700 via-purple-600 to-indigo-800 flex flex-col items-center justify-center">
       <div className="container">
+        
         <CurrencyConvertor />
+        
       </div>
     </div>
   );
